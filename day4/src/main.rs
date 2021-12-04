a220eb8af4d891d10c894345d7572a3069ed265a
David Phillips <zantier.tasa@gmail.com>
2021-12-04 10:09:26 +0000

    day4 part2 rust

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{stdin,Stdin};
 const SIZE: i32 = 5;
 
 /// A bingo board. The first index is the row.
-#[derive(Debug)]
+#[derive(Clone,Debug)]
 struct Board {
     vals: Vec<Vec<i32>>,
     marks: Vec<Vec<bool>>,
@@ -15,7 +15,7 @@ impl Board {
     fn new(vals: Vec<Vec<i32>>) -> Board {
         let vec_size = SIZE as usize;
         Board {
-            vals: vals,
+            vals,
             marks: vec![vec![false; vec_size]; vec_size],
             rows_left: vec![SIZE; vec_size],
             cols_left: vec![SIZE; vec_size],
@@ -27,7 +27,7 @@ fn main() {
     let mut input = String::new();
     let mut stdin = stdin();
     stdin.read_line(&mut input).unwrap();
-    input.pop();
+    let mut input = String::from(input.trim());
 
     let nums: Vec<i32> = input.split(',')
         .map(|s| s.parse().unwrap())
@@ -36,13 +36,17 @@ fn main() {
     // Read blank line
     stdin.read_line(&mut input).unwrap();
 
-    let mut boards = get_stdin_boards(&mut stdin);
+    let boards = get_stdin_boards(&mut stdin);
 
-    let res = solve1(&nums, &mut boards);
+    let res = solve1(&nums, &boards);
+    println!("{}", res);
+
+    let res = solve2(&nums, &boards);
     println!("{}", res);
 }
 
-fn solve1(nums: &Vec<i32>, boards: &mut Vec<Board>) -> i32 {
+fn solve1(nums: &[i32], boards: &[Board]) -> i32 {
+    let mut boards = Vec::from(boards);
     for num in nums.iter() {
         for board in boards.iter_mut() {
             for (i, row) in board.vals.iter().enumerate() {
@@ -64,6 +68,40 @@ fn solve1(nums: &Vec<i32>, boards: &mut Vec<Board>) -> i32 {
     0
 }
 
+fn solve2(nums: &[i32], boards: &[Board]) -> i32 {
+    let mut boards = Vec::from(boards);
+    let mut boards_left = boards.len();
+    let mut done = vec![false; boards.len()];
+    for num in nums.iter() {
+        for (board_index, board) in boards.iter_mut().enumerate() {
+            if done[board_index] {
+                continue;
+            }
+
+            for (i, row) in board.vals.iter().enumerate() {
+                for (j, val) in row.iter().enumerate() {
+                    if val == num {
+                        board.marks[i][j] = true;
+                        board.rows_left[i] -= 1;
+                        board.cols_left[j] -= 1;
+
+                        if board.rows_left[i] == 0 || board.cols_left[j] == 0 {
+                            done[board_index] = true;
+                            boards_left -= 1;
+
+                            if boards_left == 0 {
+                                return num * board_val(board);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    0
+}
+
 fn board_val(board: &Board) -> i32 {
     let mut res = 0;
     for (i, row) in board.vals.iter().enumerate() {
@@ -74,7 +112,7 @@ fn board_val(board: &Board) -> i32 {
         }
     }
 
-    return res;
+    res
 }
 
 fn get_stdin_boards(stdin: &mut Stdin) -> Vec<Board> {
