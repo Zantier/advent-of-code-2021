5cbf3ff1cbead0c40ea6ed868b4d6c16f048c7e3
David Phillips <zantier.tasa@gmail.com>
2021-12-07 19:02:48 +0000

    day7 part2 rust

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::io::stdin;
 
 fn main() {
@@ -11,6 +12,9 @@ fn main() {
 
     let res = solve1(&input);
     println!("{}", res);
+
+    let res = solve2(&input);
+    println!("{}", res);
 }
 
 fn solve1(input: &[i32]) -> i32 {
@@ -23,5 +27,28 @@ fn solve1(input: &[i32]) -> i32 {
     let res: i32 = input.iter()
         .map(|num| (num-mid).abs())
         .sum();
+    res
+}
+
+fn solve2(input: &[i32]) -> i32 {
+    // Binary search on horizontal position
+    let lo = *input.iter().min().unwrap();
+    let hi = *input.iter().max().unwrap();
+
+    let mut res = 1000000000;
+    for pos in lo..hi+1 {
+        let val = get_fuel(input, pos);
+        res = min(res, val);
+    }
+    res
+}
+
+/// Get the fuel needed for all crabs to get to pos
+fn get_fuel(input: &[i32], pos: i32) -> i32 {
+    let mut res = 0;
+    for val in input.iter() {
+        let val = (pos - val).abs();
+        res += (val * (val+1)) / 2;
+    }
     res
 }
