2ccc2526a71ccd159c8ea915e5d429beba98104b
David Phillips <zantier.tasa@gmail.com>
2021-12-12 15:15:40 +0000

    day12 part2 rust

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -25,6 +25,9 @@ fn main() {
 
     let res = Solve1::solve(&nodes, &graph);
     println!("{}", res);
+
+    let res = Solve2::solve(&nodes, &graph);
+    println!("{}", res);
 }
 
 struct Solve1 {
@@ -59,6 +62,61 @@ impl Solve1 {
             res += self.dfs(node2, visited);
         }
         visited.insert(node.to_string(), false);
+
+        res
+    }
+}
+
+struct Solve2 {
+    graph: HashMap<String,HashSet<String>>,
+}
+struct Solve2Mut {
+    visited: HashMap<String,i32>,
+    visited_twice: bool,
+}
+
+impl Solve2 {
+    fn solve(nodes: &HashSet<String>,
+        graph: &HashMap<String,HashSet<String>>) -> i32 {
+        let data = Solve2 {
+            graph: graph.clone(),
+        };
+        let mut data_mut = Solve2Mut {
+            visited: HashMap::from_iter(nodes.iter()
+                .map(|s| (s.to_string(), 0))),
+            visited_twice: false,
+        };
+
+        data.dfs("start", &mut data_mut)
+    }
+
+    fn dfs(&self, node: &str, data_mut: &mut Solve2Mut) -> i32 {
+        if node == "end" {
+            return 1;
+        }
+        if node == "start" && data_mut.visited[node] == 1 {
+            return 0;
+        }
+        if data_mut.visited[node] > 0 && data_mut.visited_twice {
+            return 0;
+        }
+
+
+        if data_mut.visited[node] > 0 {
+            data_mut.visited_twice = true;
+        }
+        if node.chars().next().unwrap().is_ascii_lowercase() {
+            *data_mut.visited.entry(node.to_string()).or_default() += 1;
+        }
+        let mut res = 0;
+        for node2 in self.graph[node].iter() {
+            res += self.dfs(node2, data_mut);
+        }
+
+        if data_mut.visited[node] > 1 {
+            data_mut.visited_twice = false;
+        }
+        *data_mut.visited.entry(node.to_string()).or_default() -= 1;
 
         res
     }
