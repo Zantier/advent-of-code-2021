564fb6c665873928018d3666651c53c94bc06acc
David Phillips <zantier.tasa@gmail.com>
2021-12-24 00:18:48 +0000

    day21 part2 rust

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,9 +1,21 @@
+use std::collections::HashMap;
 use std::io::stdin;
+
+#[derive(Clone,Debug,Eq,Hash,PartialEq)]
+struct State {
+    /// Player whose turn it is
+    player: usize,
+    poses: [i32;2],
+    scores: [i32;2],
+}
 
 fn main() {
     let poses = get_starting_positions();
 
     let res = solve1(poses);
+    println!("{}", res);
+
+    let res = solve2(poses);
     println!("{}", res);
 }
 
@@ -31,6 +43,64 @@ fn solve1(poses: [i32;2]) -> i32 {
     }
 
     scores[1-index] * roll_count
+}
+
+fn solve2(poses: [i32;2]) -> i64 {
+    let mut dp: HashMap<State,i64> = HashMap::new();
+    dp.insert(State {
+        player: 0,
+        poses,
+        scores: [0,0],
+    }, 1);
+
+    for total_score in 0..43 {
+        for score0 in 0..total_score+1 {
+            let scores = [score0, total_score - score0];
+            if scores[0] >= 21 || scores[1] >= 21 {
+                continue;
+            }
+            for player in 0..2 {
+                for pos0 in 1..11 {
+                    for pos1 in 1..11 {
+                        let state = State {
+                            player,
+                            poses: [pos0,pos1],
+                            scores,
+                        };
+                        if let Some(&count) = dp.get(&state) {
+                            roll_dice(&mut dp, state.clone(), count, 3);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let mut res = 0;
+    for (state,count) in dp {
+        if state.scores[0] >= 21 {
+            res += count;
+        }
+    }
+
+    res
+}
+
+fn roll_dice(dp: &mut HashMap<State,i64>, state: State, count: i64, dice: i32) {
+    if dice == 0 {
+        let mut state = state.clone();
+        state.scores[state.player] += state.poses[state.player];
+        state.player = 1 - state.player;
+        *dp.entry(state.clone()).or_insert(0) += count;
+        return;
+    }
+
+    for roll in 1..4 {
+        let mut state = state.clone();
+        state.poses[state.player] += roll;
+        state.poses[state.player] = (state.poses[state.player] - 1) % 10 + 1;
+        roll_dice(dp, state, count, dice-1);
+    }
 }
 
 fn get_starting_positions() -> [i32;2] {
