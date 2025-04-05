2cab67bb245b92d0b9bda3fba675db14a38946b3
David Phillips <zantier.tasa@gmail.com>
2025-04-05 09:04:00 +0100

    nix: Remove dependency on flake utils

--- flake.nix
+++ flake.nix
@@ -1,23 +1,24 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     # nixpkgs 21808d22b1cda1898b71cf1a1beb524a97add2c4
     #   https://lazamar.co.uk/nix-versions/?channel=nixos-unstable&package=cargo
     #   cargo 1.83, rust-analyzer 2025-01-08
     nixpkgs.url = "github:nixos/nixpkgs/21808d22b1cda1898b71cf1a1beb524a97add2c4";
   };
 
-  outputs = { flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      forEachSystem = function:
+        nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
           packages = [
             pkgs.cargo
             pkgs.rust-analyzer
           ];
         };
-      }
-    );
+      });
+    };
 }
